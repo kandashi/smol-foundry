5d9785d5135d0960729ac7ab46f82a942cb852c2
kandashi <jacobmpalmer@live.co.uk>
2024-02-05 22:01:08 +0000

    v11 update

--- scripts/condenser.js
+++ scripts/condenser.js
@@ -16,14 +16,25 @@ Hooks.once('init', async function() {
         config: true,
         onChange: refresh
     });
+    game.settings.register("smol-foundry", "compSize", {
+        name: 'Compendium Height',
+        scope: 'client',
+        type: Number,
+        default: 50,
+        config: true,
+        onChange: refresh
+    });
 });
-Hooks.on("renderSidebarDirectory", refresh)
+Hooks.on("changeSidebarTab", refresh)
 
 function refresh(){
     const AS = game.settings.get("smol-foundry", "actorSize")
     const SS = game.settings.get("smol-foundry", "sceneSize")
+    const CS = game.settings.get("smol-foundry", "compSize")
     document.querySelector("section#scenes").style.setProperty('--sidebar-scene-height', `${SS}px`);
     document.documentElement.style.setProperty('--sidebar-item-height', `${AS}px`);
+    document.documentElement.style.setProperty('--SF-comp-height', `${CS}px`);
+    
 }
 
 
